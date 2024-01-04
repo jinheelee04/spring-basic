664c1fc9ead99e12dff10627c218fcde64629dcc
jlee04 <2pro.jlee@gmail.com>
2024-01-04 12:30:56 +0900

    싱글톤 방식의 주의점

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -62,11 +62,11 @@ public class SingletonTest {
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
-        //참조값이 다른 것을 확인
+        //참조값이 같은 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
-        // memberService1 != memberService2
+        // memberService1 == memberService2
         Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
