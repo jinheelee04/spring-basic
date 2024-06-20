09890d71f483f832d69c24af1b768d029fd8adc4
jlee04 <2pro.jlee@gmail.com>
2024-06-20 10:18:13 +0900

    빈 생명주기 콜백 - 빈 등록 초기화, 소멸 메서드

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 /**
  * 스프링 빈의 이벤트 라이프사이클
  * 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
@@ -11,15 +14,18 @@ package hello.core.lifecycle;
  * 생성자는 필수 정보(파라미터)를 받고, 메모리를 할당해서 객체를 생성하는 책임을 가진다.
  * 초기화는 이렇게 생성된 값들을 활용해서 외부 컨넥션을 연결하는 등 무거운 동작을 수행한다.
  * 따라서 객체를 생성하는 부분과 초기화 하는 부분을 명확하게 나눈는 것이 유지보수 관점에서 좋다.
+ *
+ *  InitializingBean, DisposableBean 초기화, 소멸 인터페이스 단점
+ *  - 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
+ *  - 초기화, 소멸 메소드의 이름을 변경할 수 없다.
+ *  - 내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
  */
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -38,5 +44,26 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+
+    /**
+     * 의존관계 주입이 끝나면 호출
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    /**
+     * 빈 종료시 호출
+     * @throws Exception
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
