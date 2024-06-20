94f76d3832d01b1205eaa521f4dc96ae29b4612a
jlee04 <2pro.jlee@gmail.com>
2024-06-20 14:43:30 +0900

    request 스코프와 Provider

--- core/src/main/java/hello/core/spring/web/LogDemoController.java
+++ core/src/main/java/hello/core/spring/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.spring.web;
 import hello.core.spring.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,14 +19,23 @@ public class LogDemoController {
      * request 스코프 빈은 아직 생성되지 않는다. 이 빈은 실제 고객의 요청(HTTP request)이 와야 생성할 수 있다.
      * 따라서 LogDemoController가 생성되는 시점에 request가 없어서 발생하는 오류이다.
      */
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger;
+
+    /**
+     * ObjectProvider.getObject()를 LogDemoController나 LogDemoService에서 따로 호출하더라도
+     * 같은 HTTP 요청이면 같은 스프링 빈이 반환된다.
+     */
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
+
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
