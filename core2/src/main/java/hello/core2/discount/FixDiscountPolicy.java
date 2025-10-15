ece58610e07c3b3f691b54b287f180cb6e48f562
jinheelee04 <2pro.jlee@gmail.com>
2025-10-15 12:57:33 +0900

    feat: 할인 정책 클래스 추가

---
+++ core2/src/main/java/hello/core2/discount/FixDiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core2.discount;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        return member.getGrade() == Grade.VIP ? 0 : discountFixAmount;
+    }
+}
