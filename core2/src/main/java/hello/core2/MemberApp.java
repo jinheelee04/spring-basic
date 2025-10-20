6cce4603ca31c522934d4f696fd78799f270739e
jinheelee04 <2pro.jlee@gmail.com>
2025-10-20 12:50:44 +0900

    refactor: 스프링 빈 설정

--- core2/src/main/java/hello/core2/MemberApp.java
+++ core2/src/main/java/hello/core2/MemberApp.java
@@ -3,12 +3,17 @@ package hello.core2;
 import hello.core2.member.Grade;
 import hello.core2.member.Member;
 import hello.core2.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        // 스프링 컨테이너
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 스프링빈이름(default 메서드명) + 타입
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
